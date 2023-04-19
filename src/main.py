d263206eaa5a8016b41b5d8c6005f3a859566047
Jay <504jgarcia@gmail.com>
2023-04-19 12:53:16 -0500

    smaller framework

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import time
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
@@ -13,18 +14,15 @@ from MarsDataset import MarsDataset
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 3, 3, 1)
-        self.conv2 = nn.Conv2d(3, 1, 3, 1)
+        self.conv1 = nn.Conv2d(3, 1, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(12321, 512)
-        self.fc2 = nn.Linear(512, 4)
+        self.fc1 = nn.Linear(12544, 32)
+        self.fc2 = nn.Linear(32, 4)
 
 
     def forward(self, x):
         x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
@@ -150,7 +148,7 @@ def main():
         scheduler.step()
 
 
-    torch.save(model.state_dict(), "mnist_cnn.pt")
+    torch.save(model.state_dict(), str(time.time())+"mars_cnn.pt")
 
 
 if __name__ == '__main__':
